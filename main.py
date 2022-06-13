3e03243adaa408bfcece8382b399ec6e65ef1177
surumistry <57244498+surumistry@users.noreply.github.com>
2022-06-13 19:44:14 +0530

    new pg2

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-weight = int(input("Enter the weight in pounds or Kg "))
-unit = input("(L)bs or (k)g")
-if unit.upper() == "L":
-    conv = weight*0.45
-    print(conv)
-else:
-    print(weight/0.45)
-
-
+command = " "
+while command != "quit":
+    command = input("> ").lower()
+    if command == "start":
+        print("Car started...  ")
+    elif command == "stop":
+        print("Car Stoped")
+    elif command == "help":
+        print("""
+        START -- To start a car
+        STOP -- To stop a car 
+        QUIT -- To quit the gamee
+        """)
+    elif command == "quit":
+        break
+    else:
+     print("Try other command")
 
 
